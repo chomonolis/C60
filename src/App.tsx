38be704cec5efea1c312cac3fcd352ecdf1c7a19
chomonolis <tyomo3317@gmail.com>
2022-04-08 22:12:50 +0900

    Amplify UI v2版ログインを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Container } from '@mui/material';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { AmplifyProvider, Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <>
-      <Container>tst</Container>
-    </>
+    <AmplifyProvider>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </AmplifyProvider>
   );
 }
-
-export default App;
