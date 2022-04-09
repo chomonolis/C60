fe54698e0505895dd8bb796bc1a1e3716cb76a8d
chomonolis <tyomo3317@gmail.com>
2022-04-09 18:27:18 +0900

    ユーザID取得処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Amplify } from 'aws-amplify';
 
 import { AmplifyProvider, Authenticator } from '@aws-amplify/ui-react';
@@ -7,14 +8,37 @@ import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 export default function App() {
+  const [userId, setUserId] = useState<string>('');
   return (
     <AmplifyProvider>
       <Authenticator>
         {({ signOut, user }) => {
+          if (userId === '') {
+            user.getUserAttributes((err, result) => {
+              if (err) {
+                console.error(err);
+              }
+              if (result !== undefined) {
+                for (const obj of result) {
+                  if (obj.Name === 'custom:userid' && obj.Value !== userId) {
+                    setUserId(obj.Value);
+                  }
+                }
+              }
+            });
+          }
           return (
             <main>
               <h1>Hello {user.username}</h1>
-              <button onClick={signOut}>Sign out</button>
+              <h1>{userId}</h1>
+              <button
+                onClick={() => {
+                  setUserId('');
+                  signOut();
+                }}
+              >
+                Sign out
+              </button>
             </main>
           );
         }}
