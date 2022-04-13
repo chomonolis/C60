3629ac7939d98549d776f970a17070b2d17d0bf0
chomonolis <61158776+chomonolis@users.noreply.github.com>
2022-04-13 17:52:16 +0900

    Issue7:ログインユーザの情報をattributesより取得する形式に変更 (#10)
    * ユーザのユーザidについて、取得方法を変更
    
    * ワーニング対応。コンテキストの引数の処理を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from 'react';
+import { createContext } from 'react';
 import { Amplify } from 'aws-amplify';
 
 import { AmplifyProvider, Authenticator } from '@aws-amplify/ui-react';
@@ -8,6 +8,7 @@ import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 import UserData from './component/UserData';
+import { hasProperty } from './utils/typeUtils';
 
 type UserDataContextType = {
   userId: string;
@@ -15,42 +16,24 @@ type UserDataContextType = {
 export const UserDataContext = createContext<UserDataContextType>({ userId: '' });
 
 export default function App() {
-  const [userId, setUserId] = useState<string>('');
   return (
     <AmplifyProvider>
       <Authenticator>
         {({ signOut, user }) => {
-          if (userId === '') {
-            user.getUserAttributes((err, result) => {
-              if (err) {
-                console.error(err);
-              }
-              if (result !== undefined) {
-                for (const obj of result) {
-                  if (obj.Name === 'custom:userid' && obj.Value !== userId) {
-                    setUserId(obj.Value);
-                  }
-                }
-              }
-            });
+          if (user?.attributes && hasProperty(user.attributes, 'custom:userid')) {
+            if (typeof user.attributes['custom:userid'] === 'string') {
+              return (
+                <UserDataContext.Provider value={{ userId: user.attributes['custom:userid'] }}>
+                  <main>
+                    <h1>Hello {user.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                  </main>
+                  <UserData />
+                </UserDataContext.Provider>
+              );
+            }
           }
-          return (
-            <UserDataContext.Provider value={{ userId }}>
-              <main>
-                <h1>Hello {user.username}</h1>
-                <h1>{userId}</h1>
-                <button
-                  onClick={() => {
-                    setUserId('');
-                    signOut();
-                  }}
-                >
-                  Sign out
-                </button>
-              </main>
-              <UserData />
-            </UserDataContext.Provider>
-          );
+          return <>Not Authenticated</>;
         }}
       </Authenticator>
     </AmplifyProvider>
